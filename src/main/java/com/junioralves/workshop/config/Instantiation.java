0e54c498a74b6612d8f273b70c529d39d0750030
JuniorAlves05 <junioralveswork01@gmail.com>
2023-09-07 16:57:53 -0300

    Limpando o banco de dados e atualizando com novos usuarios

---
+++ src/main/java/com/junioralves/workshop/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.junioralves.workshop.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.junioralves.workshop.domain.User;
+import com.junioralves.workshop.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository; // Repositório
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll(); // Vai limpar a coleção de dados no MongoDB
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob)); // Salvando no banco de dados
+    }
+}
