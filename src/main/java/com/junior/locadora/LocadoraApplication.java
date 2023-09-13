396f504ee9c0823d330a5af6546bb835b5602734
JuniorAlves05 <junioralveswork01@gmail.com>
2023-09-13 20:34:18 -0300

    Configurando o Spring

---
+++ src/main/java/com/junior/locadora/LocadoraApplication.java
@@ -0,0 +1,16 @@
+package com.junior.locadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // Instanciando o SpringBoot
+public class LocadoraApplication {
+
+		public static void main(String[] args) {
+			SpringApplication.run(LocadoraApplication.class,args); // LocadoraApplicaton é o nome da classe 
+		 // Testar se a aplication ta funcionando na porta : 8080	
+		 // No Chrome colocar http://localhost:8080
+			
+		}
+	
+}
