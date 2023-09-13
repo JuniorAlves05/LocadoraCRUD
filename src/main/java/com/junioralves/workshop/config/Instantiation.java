4ec9bcea21a3cb5fb12499170dca84695ac5d3f3
JuniorAlves05 <junioralveswork01@gmail.com>
2023-09-13 18:45:03 -0300

    Inserindo comentarios

--- src/main/java/com/junioralves/workshop/config/Instantiation.java
+++ src/main/java/com/junioralves/workshop/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.junioralves.workshop.domain.Post;
 import com.junioralves.workshop.domain.User;
 import com.junioralves.workshop.dto.AuthorDTO;
+import com.junioralves.workshop.dto.CommentDTO;
 import com.junioralves.workshop.repository.PostRepository;
 import com.junioralves.workshop.repository.UserRepository;
 
@@ -38,13 +39,19 @@ public class Instantiation implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(maria, alex, bob)); // Salvando no banco de dados
         
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para SP. Abraços!", new AuthorDTO (maria));
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia", "Acordei feliz hoje", new AuthorDTO (maria));
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para SP. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia", "Acordei feliz hoje", new AuthorDTO(maria));
+       
+        CommentDTO c1 = new CommentDTO("Boa viagme mano",sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite",sdf.parse("22/03/2018"),new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia",sdf.parse("23/03/2018"), new AuthorDTO(alex));
 
-        postRepository.save(post1);
-        postRepository.save(post2);
+        post1.getComments().addAll(Arrays.asList(c1,c2));
+        post2.getComments().addAll(Arrays.asList(c3));
+        
+        postRepository.saveAll(Arrays.asList(post1, post2	));
     
-        maria.getPosts().addAll(Arrays.asList(post1,post2));
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
         userRepository.save(maria);
     }
 }
